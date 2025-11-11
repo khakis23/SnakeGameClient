ee25ed9f9f4d54b955a7d6f2dc7b6ae722875d3c
Tyler Black <tyler.black00@yahoo.com>
2025-11-11 12:02:20 -0700

    working debug client

--- client/include/ClientManager.h
+++ client/include/ClientManager.h
@@ -3,13 +3,97 @@
 #define SNAKECLIENT_CLIENTMANAGER_H
 #include <thread>
 #include <iostream>
+#include <IXWebSocket.h>
 
+#include "utils.h"
 
 
 class ClientManager {
 public:
+    ClientManager(const std::string &url, const std::string &room_id);
+    void run();   // no blocking
+    void stop();
+    void send(const std::string &msg);
+
+    void debugManualInput();
 private:
+    ix::WebSocket ws;   // this has to be static for ?thread?
+    std::string url;
+    std::string room_id;
+
+    int room_seat;
+
+    void handleMessage(std::string &msg);   // message will be modified
+    void startGame(int seat);
 };
+
+
+ClientManager::ClientManager(const std::string &url, const std::string &room_id) : url(url), room_id(room_id) {
+
+}
+
+void ClientManager::run() {
+    ws.setUrl(url);
+    ws.setOnMessageCallback([this](const ix::WebSocketMessagePtr& msg) {
+            using ix::WebSocketMessageType;
+
+            if (msg->type == WebSocketMessageType::Open) {
+                ws.sendText(room_id);
+                std::cout << "Connected\n";
+            }
+            else if (msg->type == WebSocketMessageType::Message) {
+                std::cout << "recv: " << msg->str << "\n";
+                if (msg->str == "send room id")
+                    ws.sendText("room1");
+            }
+            else if (msg->type == WebSocketMessageType::Error) {
+                std::cerr << "error: " << msg->errorInfo.reason << "\n";
+
+            }
+            else if (msg->type == WebSocketMessageType::Close) {
+                std::cout << "Disconnected\n";
+            }
+        });
+    ws.start();
+}
+
+void ClientManager::handleMessage(std::string &msg) {
+    auto msg_map = decodeJSON(msg);
+
+    for (auto& [key, val] : msg_map) {
+        switch (std::stoi(key)) {
+            case MOVE:
+                std::cout << "Move to: " << val << "\n";
+                break;
+            case COLLISION:
+                std::cout << "Collision: " << val << "\n";
+                break;
+            case APPLE:
+                std::cout << "New Apple: " << val << "\n";
+                break;
+            case GROW:
+                std::cout << "Snake grew\n";
+                break;
+            case START:
+                startGame(std::stoi(val));
+                break;
+        }
+    }
+}
+
+void ClientManager::startGame(int seat) {
+    room_seat = seat;
+}
+
+void ClientManager::debugManualInput() {
+    std::string key, val;
+    while (std::cin >> key >> val) {
+        ws.sendText(toJSON<int>(std::unordered_map<int, std::string>{{std::stoi(key), val}}));
+        std::cin.ignore();
+        std::cin.clear();
+    }
+}
+
 
 
 #endif //SNAKECLIENT_CLIENTMANAGER_H
---
+++ client/include/utils.h
@@ -0,0 +1,122 @@
+#ifndef SNAKECLIENT_UTIL_H
+#define SNAKECLIENT_UTIL_H
+#include <iostream>
+#include <unordered_map>
+#include <type_traits>
+
+
+enum GameCodes {
+    // To Client
+    START,
+    COLLISION,
+    APPLE,
+    GROW,
+
+    // To Server
+    MOVE,
+    READY,
+    RESET,
+};
+
+
+struct Vec2 {
+    int x;
+    int y;
+    bool operator==(const Vec2 &other) const {
+        return x == other.x && y == other.y;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Vec2& v) {
+        os << v.x << ", " << v.y;
+        return os;
+    }
+};
+
+
+template<> struct std::hash<Vec2> {
+    size_t operator()(const Vec2& v) const noexcept {
+        return (static_cast<size_t>(v.x) * 73856093u) ^ (static_cast<size_t>(v.y) * 19349663u);
+    }
+};
+
+
+template<typename T>   // so ENUM can be converted to int
+std::string toJSON(const std::unordered_map<T, std::string> &kv_pairs) {
+    if (kv_pairs.size() == 0) return "{}";
+
+    std::string json = "{";
+    for (const auto &p : kv_pairs) {
+        //             key                  value
+        json += "\"" + std::to_string(static_cast<int>(p.first))
+              + "\":\"" + p.second + "\",";
+    }
+    return json.substr(0, json.size() - 1) + "}";   // remove extra ',' char
+}
+
+
+template<typename T>
+std::unordered_map<std::string, std::string> decodeJSON(T &json) {
+    /*
+     * Decode BASIC JSON string(_view)'s -> {"key":"value", "string": "1,2"}
+     */
+    // deal with using a template and invalid types
+    std::cout << "json: " << json << std::endl;
+    try {
+        if (json.empty() || json.size() <= 2)   // yes this will probably crash if not a stringtype,
+            return {};
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Invalid string type in \"decodeJSON\": "<< e.what() << std::endl;
+        return {};
+    }
+
+    // handle remove JSON bracket { from string or string_view
+    if constexpr (std::is_same_v<std::decay_t<T>, std::string>) {
+        if (json[0] == '{')
+            json.erase(0, 1);
+    }
+    else if constexpr (std::is_same_v<std::decay_t<T>, std::string_view>) {
+        if (json.front() == '{')   // remove prefix
+            json.remove_prefix(1);
+    }
+
+    std::unordered_map<std::string, std::string> map;
+
+    // decode
+    int start = 0;
+    while (start < json.size() - 1) {
+        int mid = json.find(':', start);
+        int end = json.find("\",\"", mid);
+        // at the end of the JSON string
+        if (end == std::string::npos)
+            end = json.size() - 2;
+
+        auto key = std::string(json.substr(start + 1, mid - start - 2));
+        auto val = std::string(json.substr(mid + 2, end - mid - 2));
+
+        map[key] = val;
+        start = end + 2;
+        std::cout << key << " : " << val << "\n";   // debugging
+    }
+    return map;
+}
+
+
+Vec2 strToVec2(std::string str) {
+    std::cout << "str: " << str << std::endl;
+    int mid = str.find(',');
+    if (mid == std::string::npos)
+        throw std::runtime_error("Invalid Vec2: missing ','");
+    if (mid == 0)
+        throw std::runtime_error("Invalid Vec2: missing x or leading ','");
+
+    int x = std::stoi(
+        str.substr(0, mid)
+    );
+    int y = std::stoi(
+        str.substr(mid + 1)
+    );
+    return {x, y};
+}
+
+
+#endif //SNAKECLIENT_UTIL_H
