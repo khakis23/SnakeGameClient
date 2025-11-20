09d275e54a7645f17db3e6380c26b03991840363
Tyler Black <tyler.black00@yahoo.com>
2025-11-19 19:33:08 -0700

    comments for Blaine

--- client/src/SystemManager.cpp
+++ client/src/SystemManager.cpp
@@ -1,7 +1,7 @@
 #include "SystemManager.h"
 
 
-SystemManager::SystemManager() {}
+SystemManager::SystemManager() = default;
 
 void SystemManager::run() {
     // TODO placeholder for getting room ID
@@ -25,7 +25,24 @@ void SystemManager::run() {
     std::cout << "Websocket Running..." << std::endl;
 
     // start game (blocking)
-    client.debugManualInput();
-    // DebugGame game(incoming, outgoing);    // TODO THIS IS WHERE THE GAME GOES
-    // game.run();
+    // client.debugManualInput();
+
+    /*
+     * TODO
+     *  This is the old way of doing this, but I have a much cleaner and scalable idea...
+     *
+     *      // OLD WAY
+     *      SnakeGame game(incoming, outgoing);
+     *      game.run();   // blocking, no way to client.send()
+     *
+     *      // NEW WAY
+     *      SnakeGame game(incoming, outgoing);
+     *      while (game.running) {  // yes, make this a public variable since we are checking it so often, much faster than calling a method
+     *          game.update();
+     *          game...             // anything else it might (or not) need to do
+     *          game.tick(FPS);     // ...or whatever RayLib uses (may want a getFPS() and before this while loop set FPS=game.getFPS(); )
+     *          client.send();      // client has a reference to outgoing, this will send everything game pushed to outgoing
+     *       }
+     *       game.stop();    // this is crutial since game no longer knows if it's running or not
+     */
 }
